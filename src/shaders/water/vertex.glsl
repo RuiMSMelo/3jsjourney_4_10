09105db4de80ac817eb5069a9d1e02a58910149d
RuiMSMelo <ruimsmelo@gmail.com>
2025-05-06 13:25:27 +0100

    computing mormals

--- src/shaders/water/vertex.glsl
+++ src/shaders/water/vertex.glsl
@@ -9,25 +9,29 @@ uniform float uSmallWavesSpeed;
 uniform float uSmallIterations;
 
 varying float vElevation;
+varying vec3 vNormal;
+varying vec3 vPosition;
 
 #include ../includes/perlinClassic3D.glsl
+#include waveElevation.glsl
 
 void main()
 {
     // Base position
+    float shift = 0.01;
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec3 modelPositionA = modelPosition.xyz + vec3(shift, 0.0, 0.0);
+    vec3 modelPositionB = modelPosition.xyz + vec3(0.0, 0.0, - shift);
 
     // Elevation
-    float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) *
-                      sin(modelPosition.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) *
-                      uBigWavesElevation;
-
-    for(float i = 1.0; i <= uSmallIterations; i++)
-    {
-        elevation -= abs(perlinClassic3D(vec3(modelPosition.xz * uSmallWavesFrequency * i, uTime * uSmallWavesSpeed)) * uSmallWavesElevation / i);
-    }
-    
+    float elevation = waveElevation(modelPosition.xyz);
     modelPosition.y += elevation;
+    modelPositionA.y += waveElevation(modelPositionA);
+    modelPositionB.y += waveElevation(modelPositionB);
+
+    // Compute normal
+    vec3 toA = normalize(modelPositionA - modelPosition.xyz);
+    vec3 toB = normalize(modelPositionB - modelPosition.xyz);
 
     // Final position
     vec4 viewPosition = viewMatrix * modelPosition;
@@ -36,4 +40,6 @@ void main()
 
     // Varyings
     vElevation = elevation;
+    vNormal = (modelMatrix * vec4(normal, 0.0)).xyz;
+    vPosition = modelPosition.xyz;
 }

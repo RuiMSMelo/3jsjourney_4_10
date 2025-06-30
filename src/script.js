99be0e94232f1a030de89383d62e61a20683bb05
RuiMSMelo <ruimsmelo@gmail.com>
2025-06-30 13:16:11 +0100

    small tweaks

--- src/script.js
+++ src/script.js
@@ -35,97 +35,87 @@ debugObject.depthColor = '#ff4000'
 debugObject.surfaceColor = '#151c37'
 
 gui.addColor(debugObject, 'depthColor').onChange(() => {
-  waterMaterial.uniforms.uDepthColor.value.set(debugObject.depthColor)
+    waterMaterial.uniforms.uDepthColor.value.set(debugObject.depthColor)
 })
 gui.addColor(debugObject, 'surfaceColor').onChange(() => {
-  waterMaterial.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor)
+    waterMaterial.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor)
 })
 
 // Material
 const waterMaterial = new THREE.ShaderMaterial({
-  vertexShader: waterVertexShader,
-  fragmentShader: waterFragmentShader,
-  uniforms: {
-    uTime: { value: 0 },
+    vertexShader: waterVertexShader,
+    fragmentShader: waterFragmentShader,
+    uniforms: {
+        uTime: { value: 0 },
 
-    uBigWavesElevation: { value: 0.2 },
-    uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
-    uBigWavesSpeed: { value: 0.75 },
+        uBigWavesElevation: { value: 0.2 },
+        uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
+        uBigWavesSpeed: { value: 0.75 },
 
-    uSmallWavesElevation: { value: 0.15 },
-    uSmallWavesFrequency: { value: 3 },
-    uSmallWavesSpeed: { value: 0.2 },
-    uSmallIterations: { value: 4 },
+        uSmallWavesElevation: { value: 0.15 },
+        uSmallWavesFrequency: { value: 3 },
+        uSmallWavesSpeed: { value: 0.2 },
+        uSmallIterations: { value: 4 },
 
-    uDepthColor: { value: new THREE.Color(debugObject.depthColor) },
-    uSurfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
-    uColorOffset: { value: 0.925 },
-    uColorMultiplier: { value: 1 },
-  },
+        uDepthColor: { value: new THREE.Color(debugObject.depthColor) },
+        uSurfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
+        uColorOffset: { value: 0.925 },
+        uColorMultiplier: { value: 1 },
+    },
 })
 
-gui
-  .add(waterMaterial.uniforms.uBigWavesElevation, 'value')
-  .min(0)
-  .max(1)
-  .step(0.001)
-  .name('uBigWavesElevation')
-gui
-  .add(waterMaterial.uniforms.uBigWavesFrequency.value, 'x')
-  .min(0)
-  .max(10)
-  .step(0.001)
-  .name('uBigWavesFrequencyX')
-gui
-  .add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y')
-  .min(0)
-  .max(10)
-  .step(0.001)
-  .name('uBigWavesFrequencyY')
-gui
-  .add(waterMaterial.uniforms.uBigWavesSpeed, 'value')
-  .min(0)
-  .max(4)
-  .step(0.001)
-  .name('uBigWavesSpeed')
+gui.add(waterMaterial.uniforms.uBigWavesElevation, 'value')
+    .min(0)
+    .max(1)
+    .step(0.001)
+    .name('uBigWavesElevation')
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'x')
+    .min(0)
+    .max(10)
+    .step(0.001)
+    .name('uBigWavesFrequencyX')
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y')
+    .min(0)
+    .max(10)
+    .step(0.001)
+    .name('uBigWavesFrequencyY')
+gui.add(waterMaterial.uniforms.uBigWavesSpeed, 'value')
+    .min(0)
+    .max(4)
+    .step(0.001)
+    .name('uBigWavesSpeed')
 
-gui
-  .add(waterMaterial.uniforms.uSmallWavesElevation, 'value')
-  .min(0)
-  .max(1)
-  .step(0.001)
-  .name('uSmallWavesElevation')
-gui
-  .add(waterMaterial.uniforms.uSmallWavesFrequency, 'value')
-  .min(0)
-  .max(30)
-  .step(0.001)
-  .name('uSmallWavesFrequency')
-gui
-  .add(waterMaterial.uniforms.uSmallWavesSpeed, 'value')
-  .min(0)
-  .max(4)
-  .step(0.001)
-  .name('uSmallWavesSpeed')
-gui
-  .add(waterMaterial.uniforms.uSmallIterations, 'value')
-  .min(0)
-  .max(5)
-  .step(1)
-  .name('uSmallIterations')
+gui.add(waterMaterial.uniforms.uSmallWavesElevation, 'value')
+    .min(0)
+    .max(1)
+    .step(0.001)
+    .name('uSmallWavesElevation')
+gui.add(waterMaterial.uniforms.uSmallWavesFrequency, 'value')
+    .min(0)
+    .max(30)
+    .step(0.001)
+    .name('uSmallWavesFrequency')
+gui.add(waterMaterial.uniforms.uSmallWavesSpeed, 'value')
+    .min(0)
+    .max(4)
+    .step(0.001)
+    .name('uSmallWavesSpeed')
+gui.add(waterMaterial.uniforms.uSmallIterations, 'value')
+    .min(0)
+    .max(5)
+    .step(1)
+    .name('uSmallIterations')
 
-gui
-  .add(waterMaterial.uniforms.uColorOffset, 'value')
-  .min(0)
-  .max(1)
-  .step(0.001)
-  .name('uColorOffset')
-gui
-  .add(waterMaterial.uniforms.uColorMultiplier, 'value')
-  .min(0)
-  .max(10)
-  .step(0.001)
-  .name('uColorMultiplier')
+gui.add(waterMaterial.uniforms.uColorOffset, 'value')
+    .min(0)
+    .max(1)
+    .step(0.001)
+    .name('uColorOffset')
+gui.add(waterMaterial.uniforms.uColorMultiplier, 'value')
+    .min(0)
+    .max(10)
+    .step(0.001)
+    .name('uColorMultiplier')
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
@@ -136,22 +126,22 @@ scene.add(water)
  * Sizes
  */
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
+    width: window.innerWidth,
+    height: window.innerHeight,
 }
 
 window.addEventListener('resize', () => {
-  // Update sizes
-  sizes.width = window.innerWidth
-  sizes.height = window.innerHeight
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
-  // Update camera
-  camera.aspect = sizes.width / sizes.height
-  camera.updateProjectionMatrix()
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height)
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -159,10 +149,10 @@ window.addEventListener('resize', () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100
 )
 camera.position.set(1, 1, 1)
 scene.add(camera)
@@ -175,7 +165,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
+    canvas: canvas,
 })
 renderer.toneMapping = THREE.ACESFilmicToneMapping
 renderer.setSize(sizes.width, sizes.height)
@@ -187,19 +177,19 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 const clock = new THREE.Clock()
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime()
+    const elapsedTime = clock.getElapsedTime()
 
-  // Water
-  waterMaterial.uniforms.uTime.value = elapsedTime
+    // Water
+    waterMaterial.uniforms.uTime.value = elapsedTime
 
-  // Update controls
-  controls.update()
+    // Update controls
+    controls.update()
 
-  // Render
-  renderer.render(scene, camera)
+    // Render
+    renderer.render(scene, camera)
 
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick)
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
 }
 
 tick()
